00df5786c530e873b03533b37c01b19bd7015123
jkwon <kwonejk@gmail.com>
2022-06-11 14:46:39 +0800

    nweet form

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [nweet, setNweet] = useState(""); 
+    const onSubmit = (event) => {
+        event.preventDefault(); 
+    };
+    const onChange = (event) => {
+        const {target : { value }} = event;
+        setNweet(value); 
+    };
+    
     return (
-        <div>Home</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input vlaue={nweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120} />
+                <input type="submit" value="Send" />
+            </form>
+        </div>
     )
 };
 
